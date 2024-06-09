6569bc307cf95ebe9ec310cc78f16c6569b31fd5
yifancai <eve.yifan.cai@gmail.com>
2024-06-09 14:34:21 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import cors from "cors";
+import express from "express";
+//const express = require("express");
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5/index.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+const app = express();
+app.use(cors());
+app.use(express.json());
+Hello(app);
+Lab5(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+app.listen(process.env.PORT || 4000);
---
+++ Kanbas/Database/index.js
@@ -0,0 +1,7 @@
+import courses from "./courses.js";
+import assignments from "./assignments.js";
+import enrollments from "./enrollments.js";
+import grades from "./grades.js";
+import modules from "./modules.js";
+import user from "./user.js";
+export default { courses, assignments, enrollments, grades, modules, user };
